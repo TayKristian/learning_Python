90a2e2e85b499f1415c75aa8258ab7a6387d1d14
TayKristian <tayk.lima18@gmail.com>
2023-05-30 19:32:45 -0300

    aula 04

--- Aulas/aula04.py
+++ Aulas/aula04.py
@@ -1,1 +1,16 @@
-print("olá, mundo!")
+"""
+print('olá, mundo!')
+print(7+4)
+print('7'+'4')
+"""
+
+"""
+nome = 'Tay Kristian'
+idade = 26
+peso = 62.7
+"""
+
+nome = input('Qual seu nome? ')
+idade = input('Qual a sua idade? ')
+peso = input('Qual o seu peso? ')
+print(nome, idade, peso)
